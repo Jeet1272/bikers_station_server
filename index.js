7b97aada36030f0908d6d664f475fba3c76f823d
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-11-24 21:08:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f7pydfc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', async (req, res) => {
+    res.send('bikers station server is running')
+})
+
+app.listen(port, () => console.log(`server is running on ${port} `))
