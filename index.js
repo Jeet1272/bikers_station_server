ab4d6b263a9db32a78f0e304b366ea4cf4cf0b48
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-11-25 21:05:20 +0600

    category route created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -14,6 +14,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f7pydfc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const categoriesCollection = client.db('bikers-station').collection('categories');
+        const productCollection = client.db('bikers-station').collection('products')
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { category_id: parseInt(id) };
+            const result = await productCollection.find(query).toArray();
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', async (req, res) => {
     res.send('bikers station server is running')
